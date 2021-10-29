056a4b49a4746a9691ceb53c29f7a605976d1b60
Mikhail <jackglowpetux@gmail.com>
2021-10-29 09:02:12 +0500

    Add XML docs to the existing methods and fields

--- DiscordRPCCore.cs
+++ DiscordRPCCore.cs
@@ -2,17 +2,43 @@
 
 namespace DiscordRPC
 {
+	/// <summary>
+	/// Methods and fields that are mostly for internal use.
+	/// </summary>
 	public static class Utility
 	{
-		// https://discord.com/developers/docs/topics/rpc#rpc-server-ports
-		public static short[] PossiblePorts = { 6463, 6464, 6465, 6466, 6467, 6468, 6469, 6470, 6471, 6472 };
+		/// <summary>
+		/// Accordging to the <see href="https://discord.com/developers/docs/topics/rpc#rpc-server-ports">Discord RPC documentation</see> port range is [6463..6472].
+		/// </summary>
+		public readonly static short[] PossiblePorts = { 6463, 6464, 6465, 6466, 6467, 6468, 6469, 6470, 6471, 6472 };
+
+		/// <summary>
+		/// Refer to <see href="https://discord.com/developers/docs/topics/rpc#rpc-rpc-versions">Discord RPC documentation</see> in order to know which verison is present.
+		/// </summary>
+		public readonly static short Version = 1;
+
+		/// <summary>
+		/// Discord Application ID
+		/// Use <see href="https://discord.com/developers/applications">Discord Developers Portal</see> to create and obtain it.
+		/// </summary>
+		public readonly static long ClientID;
+		
+		/// <summary>
+		/// Generates a random UUID string for Discord RPC payloads.
+		/// </summary>
+		/// <returns>Random UUID string</returns>
 		public static string GetNonce()
 		{
 			return System.Guid.NewGuid().ToString( "B" )[1..^1];
 		}
 	}
+
 	public class Core
 	{
+		/// <summary>
+		/// Discord RPC command enum, used in creating payload (<see cref="DiscordRPC.Core.Payload"/>)
+		/// <para>Reference: <see href="https://discord.com/developers/docs/topics/rpc#commands-and-events-rpc-commands"/></para>
+		/// </summary>
 		public enum Command
 		{
 			DISPATCH,
@@ -33,6 +59,10 @@ namespace DiscordRPC
 			SEND_ACTIVITY_JOIN_INVITE,
 			CLOSE_ACTIVITY_REQUEST
 		}
+
+		/// <summary>
+		/// <para>Reference: <see href="https://discord.com/developers/docs/topics/rpc#commands-and-events-rpc-events"/></para>
+		/// </summary>
 		public enum RPCEvent
 		{
 			NONE = -1,
@@ -56,6 +86,12 @@ namespace DiscordRPC
 			ACTIVITY_SPECTATE,
 			ACTIVITY_JOIN_REQUEST
 		}
+
+		/// <summary>
+		/// Structure of command sent to the Discord RPC in order to perform action. <br/>
+		/// Field nonce must be unique randomly generated string. Recommended to use <see cref="DiscordRPC.Utility.GetNonce"/>.
+		/// <para>Reference: <see href="https://discord.com/developers/docs/topics/rpc#payloads"/></para>
+		/// </summary>
 		public struct Payload
 		{
 			Command cmd;
